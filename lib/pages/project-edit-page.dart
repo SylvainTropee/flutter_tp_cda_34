8e4c36097d70fa764ee32f85cf86fa1c32379186
stropee <stropee@campus-eni.fr>
2025-12-18 13:48:39 +0100

    TP 4

--- lib/pages/project-edit-page.dart
+++ lib/pages/project-edit-page.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:manahattan_cda_34/common/contribution-form.dart';
 
 import '../models/project.dart';
 
 class ProjectEditPage extends StatelessWidget {
   final Project project;
+
   const ProjectEditPage({required this.project, super.key});
+
+  void _onUpdate(Project project, BuildContext context) {
+    context.go("/");
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text("Projet ${project.title} édité !")));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(project.title)),
-      body: ContributionForm(onSubmitForm: (Project) {}, project : project),
+      body: ContributionForm(
+        onSubmitForm: (project) {
+          _onUpdate(project, context);
+        },
+        project: project,
+      ),
     );
   }
 }
